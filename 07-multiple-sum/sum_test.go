79c05dc43c9ac91cdfca1d52d6cb630e125ae889
Mateen bagheri <mateenbagheri@Mateens-MacBook-Air.local>
2023-01-14 10:37:52 +0330

    refactored test

--- 07-multiple-sum/sum_test.go
+++ 07-multiple-sum/sum_test.go
@@ -3,15 +3,17 @@ package main
 import "testing"
 
 func TestMultipleSum(t *testing.T) {
-	numbers := []int{1, 2, 3, 4, 5}
-	got := multiple_sum(numbers)
-	want := 15
-	assertCorrectMessage(t, got, want)
+	t.Run("testing the function multiple_sum by giving array to it", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := multiple_sum(numbers)
+		want := 15
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got int, want int) {
 	t.Helper()
 	if got != want {
-		t.Errorf("wanted %d but got number %d instead", want, got)
+		t.Errorf("wanted %d but got number %n instead", want, got)
 	}
 }
